c8381ad6cdb8728c5de821e215036ac9429e334b
Mbaliqwema94 <129956817+Mbaliqwema94@users.noreply.github.com>
2023-09-13 14:05:48 +0200

    defining the path of each page

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,10 +1,23 @@
 import React from "react";
+import "./index.css";
+
+import home  from "./routes/home";
+import contacts from "./routes/contacts";
+import about from "./routes/about";
+import projects from "./routes/projects";
+import { Route, Routes } from "react-router-dom";
+
 
 function App() {
   return (
-    <div >
-      
-    </div>
+    <>
+    <Routes>
+      <Route path="/" element={<home/>}/>
+      <Route path="/" contacts={<contacts/>}/>
+      <Route path="/" about={<about/>}/>
+      <Route path="/" projects={<projects/>}/>
+    </Routes>
+    </>
   );
 }
 
